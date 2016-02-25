cf68acd86c702e0562a322802e6e8c0047b1c07b
Summer Barton-Taylor <summerbartontaylor@gmail.com>
2016-02-24 18:20:47 -0600

    bootstrap and more angularJS

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-angular.module('myApp', [])
+(function () {
+    var app = angular.module('myApp', [])
+    app.controller('wordCollection', function () {
+        this.words = madLib;
+    });
+    var madLib = {
+        flag: 'FLAG',
+        usa: 'USA',
+        republic: 'REPUBLIC',
+        nation: 'NATION',
+        god: 'GOD',
+        all: 'ALL',
+        create: false
+    }
+})();
